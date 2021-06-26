d770b0b9cff367f4b9fda7054fb2b3262db56ce4
kapigames <kapigamesdev@gmail.com>
2021-06-27 01:32:27 +0200

    Move kstd function declarations to header file

--- KNoise/kstd.hpp
+++ KNoise/kstd.hpp
@@ -15,17 +15,17 @@ namespace kstd {
     // -------
 
     struct vec4f {
-        vec4f(float F_x,float F_y,float F_z,float F_w);
+        vec4f(float F_x,float F_y,float F_z,float F_w) { x=F_x; y=F_y; z=F_z; w=F_w; }
         float x, y, z, w;
     };
 
     struct vec3f {
-        vec3f(float F_x,float F_y,float F_z);
+        vec3f(float F_x,float F_y,float F_z) { x=F_x; y=F_y; z=F_z; }
         float x, y, z;
     };
 
     struct vec2f {
-        vec2f(float F_x,float F_y);
+        vec2f(float F_x,float F_y) { x=F_x; y=F_y; }
         float x, y;
     };
 
@@ -35,17 +35,17 @@ namespace kstd {
     // -------
 
     struct vec4d {
-        vec4d(double F_x,double F_y,double F_z,double F_w);
+        vec4d(double F_x,double F_y,double F_z,double F_w) { x=F_x; y=F_y; z=F_z; w=F_w; }
         double x, y, z, w;
     };
 
     struct vec3d {
-        vec3d(double F_x,double F_y,double F_z);
+        vec3d(double F_x,double F_y,double F_z) { x=F_x; y=F_y; z=F_z; }
         double x, y, z;
     };
 
     struct vec2d {
-        vec2d(double F_x,double F_y);
+        vec2d(double F_x,double F_y) { x=F_x; y=F_y; }
         double x, y;
     };
 }
