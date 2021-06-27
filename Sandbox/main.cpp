9dfd7964a783e2fdceb3f0670d0df4b6ab745b86
kapigames <kapigamesdev@gmail.com>
2021-06-27 02:38:55 +0200

    Visualize Noise in sandbox

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -4,9 +4,64 @@
 int main() {
     KNoise::Perlin Noise;
 
-    for (size_t x = 0; x < 100000; x++)
+    int x = 0;
+
+    while (true)
     {
-        std::cout << Noise.Get(kstd::vec3f(x/100.f,0,0), 0) << " double: " << Noise.Get(kstd::vec3d(x/100.0,0,0), 0) << std::endl;
+
+        for (size_t y = 0; y < 100; y++)
+        {
+            
+            float a = Noise.Get(kstd::vec3f(x/20.f, y/20.f, 0.0), 0);
+            int b = int(a * 10.f);
+
+            switch (b)
+            {
+            case 0:
+                std::cout << " ";
+                break;
+            case 1:
+                std::cout << ".";
+                break;
+            case 2:
+                std::cout << ":";
+                break;
+            case 3:
+                std::cout << "-";
+                break;
+            case 4:
+                std::cout << "!";
+                break;
+            case 5:
+                std::cout << "+";
+                break;
+            case 6:
+                std::cout << "*";
+                break;
+            case 7:
+                std::cout << "#";
+                break;
+            case 8:
+                std::cout << "%";
+                break;
+            case 9:
+                std::cout << "@";
+                break;
+            case 10:
+                std::cout << "▮";
+                break;
+            
+            default:
+                break;
+            }
+
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << std::endl;
+        x++;
+        
     }
+    
     
 }
