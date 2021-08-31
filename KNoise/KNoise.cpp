21832b56e36cc0a6605f1aee75b57f63bcf29fe0
kapigames <kapigamesdev@gmail.com>
2021-08-31 20:57:05 +0200

    ToSeed for all

--- KNoise/KNoise.cpp
+++ KNoise/KNoise.cpp
@@ -1,40 +1,47 @@
 #include "KNoise.hpp"
 
 
-unsigned int KNoise::ToSeed(std::string F_Text) {
-    std::wstring TextW( F_Text.begin(), F_Text.end() );
-    std::string Text( TextW.begin(), TextW.end() );
 
-    unsigned int Seed = 0;
+unsigned int KNoise::ToSeed(std::string F_Text) { 
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert32;
+    std::u32string utf32 = convert32.from_bytes(F_Text);
 
-    for (size_t i = 0; i < Text.length(); i++) {
-        if(Text.at(i) >= '0' && Text.at(i) <= '9') {
-            Seed = Seed * 10;
-            Seed = Seed + (Text.at(i)-48);
-        } else {
-            Seed = Seed * 100;
-            srand((int)Text.at(i)+i);
-            Seed = Seed + rand()%99;
-        }
-    }
-
-    return Seed;
+    return ToSeed(utf32);
 }
 
 
+unsigned int KNoise::ToSeed(std::wstring F_Text) {
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>,wchar_t> convert; 
+    std::string utf8 = convert.to_bytes(F_Text);
 
-unsigned int KNoise::ToSeedW(std::wstring F_Text) {
-    std::string Text = std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(F_Text);
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert32;
+    std::u32string utf32 = convert32.from_bytes(utf8);
 
+    return ToSeed(utf32);
+}
+
+
+unsigned int KNoise::ToSeed(std::u16string F_Text) { 
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> convert; 
+    std::string utf8 = convert.to_bytes(F_Text);
+
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert32;
+    std::u32string utf32 = convert32.from_bytes(utf8);
+
+    return ToSeed(utf32);
+}
+
+
+unsigned int KNoise::ToSeed(std::u32string F_Text) {
     unsigned int Seed = 0;
 
-    for (size_t i = 0; i < Text.length(); i++) {
-        if(Text.at(i) >= '0' && Text.at(i) <= '9') {
+    for (size_t i = 0; i < F_Text.length(); i++) {
+        if(F_Text.at(i) >= '0' && F_Text.at(i) <= '9') {
             Seed = Seed * 10;
-            Seed = Seed + (Text.at(i)-48);
+            Seed = Seed + (F_Text.at(i)-48);
         } else {
             Seed = Seed * 100;
-            srand((int)Text.at(i)+i);
+            srand((int)F_Text.at(i)+i);
             Seed = Seed + rand()%99;
         }
     }
--- KNoise/KNoise.hpp
+++ KNoise/KNoise.hpp
@@ -11,7 +11,10 @@
 namespace KNoise {
 
     unsigned int ToSeed(std::string F_Text);        // Converts string to seed
-    unsigned int ToSeedW(std::wstring F_Text);      // Converts wstring to seed
+    unsigned int ToSeed(std::wstring F_Text);       // Converts wstring to seed
+    unsigned int ToSeed(std::u16string F_Text);     // Converts u16string to seed
+    unsigned int ToSeed(std::u32string F_Text);     // Converts u32string to seed
+
 
     struct Vec3f {
         Vec3f(float F_x,float F_y,float F_z) { x=F_x; y=F_y; z=F_z; }
